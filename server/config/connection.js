cd005828f7765c278667baf88915cb048631c734
Miles Wildmore <mileswildmore18@gmail.com>
2024-06-15 18:00:34 -0400

    Added a shelterDB to connect to the config connection to connect ot the MongoDB. Also fixed up the seed js file to connect to the database and the server. Did a minor edit on the Pet model and typeDefs from image to animal type.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/WILD");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/shelter-the-wild");
 
 module.exports = mongoose.connection;
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,28 +2,56 @@ const db = require("../config/connection");
 const { User, Pet } = require("../models");
 const petSeeds = require("./petSeeds.json");
 const userSeeds = require("./userSeeds.json");
+const shelterDB = require('./shelterDB');
 
-const seedDatabase = async () => {
-  await db.sync({ force: true });
 
-  // users
-  const users = await User.bulkCreate(userSeeds, {
-    individualHooks: true,
-    returning: true,
-  });
+db.once('open', async () => {
+  try {
+    await shelterDB('Pet', 'pets');
+    await shelterDB('User', 'users');
 
-  // pets
-  const pets = await Pet.bulkCreate(
-    petSeeds.map((pet) => ({
-      ...pet,
-      owner_id: users[Math.floor(Math.random() * users.length)].id,
-    })),
-    {
-      returning: true,
+    await User.create(userSeeds);
+
+    for (let i = 0; i < petSeeds.length; i++) {
+      const { _id, petOwner } = await Pet.create(petSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: petOwner },
+        {
+          $addToSet: {
+            pets: _id,
+          },
+        }
+      );
     }
-  );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
+  console.log('complete!');
   process.exit(0);
-};
+});
+// const seedDatabase = async () => {
+//   //await db.sync({ force: true });
 
-seedDatabase();
+//   // users
+//   const users = await User.bulkCreate(userSeeds, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+//   // pets
+//   const pets = await Pet.bulkCreate(
+//     petSeeds.map((pet) => ({
+//       ...pet,
+//       owner_id: users[Math.floor(Math.random() * users.length)].id,
+//     })),
+//     {
+//       returning: true,
+//     }
+//   );
+
+//   process.exit(0);
+// };
+
+// seedDatabase();
