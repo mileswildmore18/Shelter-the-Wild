7cb68f420fa9d9ce00d134556e850e521e880898
walker <spadonival7@gmail.com>
2024-06-16 22:08:00 -0400

    Importing an image of a cat and assigning it to animal1

--- client/src/pages/PetBlog/index.jsx
+++ client/src/pages/PetBlog/index.jsx
@@ -1,2 +1,3 @@
 import React from 'react'; // Importing the React library
 import { Container, Grid, Card, CardContent, CardMedia, Typography } from '@mui/material'; // Importing specific components from Material-UI
+import animal1 from '../Images/cat.jpg'; // Importing an image of a cat and assigning it to `animal1`
