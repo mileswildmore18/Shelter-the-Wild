6adbb55e97670f57e91be276c055ab0f84f86b1b
walker <spadonival7@gmail.com>
2024-06-16 22:03:55 -0400

    imported comoponents from material Ui

--- client/src/pages/PetBlog/index.jsx
+++ client/src/pages/PetBlog/index.jsx
@@ -1,1 +1,2 @@
 import React from 'react'; // Importing the React library
+import { Container, Grid, Card, CardContent, CardMedia, Typography } from '@mui/material'; // Importing specific components from Material-UI
