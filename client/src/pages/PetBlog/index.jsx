4daea5dfa91fa535883e0de1e262a9d1ee838a25
walker <spadonival7@gmail.com>
2024-06-16 22:13:26 -0400

    caught another image of a pitbull and assigns it to animal3

--- client/src/pages/PetBlog/index.jsx
+++ client/src/pages/PetBlog/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; // Importing the React library
 import { Container, Grid, Card, CardContent, CardMedia, Typography } from '@mui/material'; // Importing specific components from Material-UI
 import animal1 from '../Images/cat.jpg'; // Importing an image of a cat and assigning it to `animal1`
-import animal2 from '../Images/corgi.jpg'; // Importing an image of a corgi and assigns it to `animal2`
+import animal2 from '../Images/corgi.jpg'; // Importing an image of a corgi and assigned it to `animal2`
+import animal3 from '../Images/pitbull.jpg'; // caught another image of a pitbull and assigned it to `animal3`
